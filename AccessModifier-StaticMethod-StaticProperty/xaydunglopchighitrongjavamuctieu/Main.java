536e11b2a3e4123e7ee5da39ecb30a28d6294ca1
lichnguyen0 <fakesangmy@gmail.com>
2025-09-16 13:50:54 +0700

    update

---
+++ AccessModifier-StaticMethod-StaticProperty/xaydunglopchighitrongjavamuctieu/Main.java
@@ -0,0 +1,12 @@
+package xaydunglopchighitrongjavamuctieu;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.setName("Lich nguyen");
+        student.setClasses("CodeGym");
+
+        System.out.println(student);
+    }
+}
---
+++ AccessModifier-StaticMethod-StaticProperty/xaydunglopchighitrongjavamuctieu/Student.java
@@ -0,0 +1,26 @@
+package xaydunglopchighitrongjavamuctieu;
+
+public class Student {
+
+    public String name = "John";
+    public String classes = "CO2";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
