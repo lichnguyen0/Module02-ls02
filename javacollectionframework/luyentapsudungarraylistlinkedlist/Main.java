d9f5de53ff4a663914d7f075ee81a8ec492b5c30
lichnguyen0 <fakesangmy@gmail.com>
2025-09-26 09:43:53 +0700

    update

--- javacollectionframework/luyentapsudungarraylistlinkedlist/Main.java
+++ javacollectionframework/luyentapsudungarraylistlinkedlist/Main.java
@@ -17,7 +17,7 @@ public class Main {
             System.out.println("2. Sửa thông tin theo sản phẩm theo id.");
             System.out.println("3. Xoá sản phẩm theo id.");
             System.out.println("4. Hiển thị danh sách theo sản phẩm.");
-            System.out.println("5. Tìm kiếm sản phẩm heo tên.");
+            System.out.println("5. Tìm kiếm sản phẩm theo tên.");
             System.out.println("6. Sắp xếp sane phẩm tăng dần.");
             System.out.println("7. Sắp xếp sản phẩm giảm dần.");
             System.out.println("0. Thoát chương trình");
